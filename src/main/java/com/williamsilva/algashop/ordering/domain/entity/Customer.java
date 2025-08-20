267b9162adb35244b236e7066cbe9323bc441586
William Silva <williamsilvaec@gmail.com>
2025-08-19 21:06:44 -0300

    feat: implementar customer + testes de unidade e validações

---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/entity/Customer.java
@@ -0,0 +1,202 @@
+package com.williamsilva.algashop.ordering.domain.entity;
+
+import com.williamsilva.algashop.ordering.domain.validator.FieldValidations;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.williamsilva.algashop.ordering.domain.exception.ErrorMessages.*;
+
+public class Customer {
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email,
+                    String phone, String document, Boolean promotionNotificationsAllowed,
+                    OffsetDateTime registeredAt) {
+        this.setId(id);
+        this.setFullName(fullName);
+        this.setBirthDate(birthDate);
+        this.setEmail(email);
+        this.setPhone(phone);
+        this.setDocument(document);
+        this.setPromotionNotificationsAllowed(promotionNotificationsAllowed);
+        this.setRegisteredAt(registeredAt);
+        this.setArchived(false);
+        this.setLoyaltyPoints(0);
+    }
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone,
+                    String document, Boolean promotionNotificationsAllowed, Boolean archived,
+                    OffsetDateTime registeredAt, OffsetDateTime archivedAt, Integer loyaltyPoints) {
+        this.setId(id);
+        this.setFullName(fullName);
+        this.setBirthDate(birthDate);
+        this.setEmail(email);
+        this.setPhone(phone);
+        this.setDocument(document);
+        this.setPromotionNotificationsAllowed(promotionNotificationsAllowed);
+        this.setArchived(archived);
+        this.setRegisteredAt(registeredAt);
+        this.setArchivedAt(archivedAt);
+        this.setLoyaltyPoints(loyaltyPoints);
+    }
+
+    public void addLoayltyPoints(Integer points) {
+
+    }
+
+    public void archive() {
+
+    }
+
+    public void enablePromotionNotifications() {
+        this.setPromotionNotificationsAllowed(true);
+    }
+
+    public void disablePromotionNotifications() {
+        this.setPromotionNotificationsAllowed(false);
+    }
+
+    public void changeName(String fullName) {
+        this.setFullName(fullName);
+    }
+
+    public void changeEmail(String email) {
+        this.setEmail(email);
+    }
+
+    public void changePhone(String phone) {
+        this.setPhone(phone);
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String fullName() {
+        return fullName;
+    }
+
+    public LocalDate birthDate() {
+        return birthDate;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String phone() {
+        return phone;
+    }
+
+    public String document() {
+        return document;
+    }
+
+    public Boolean isPromotionNotificationsAllowed() {
+        return promotionNotificationsAllowed;
+    }
+
+    public Boolean isArchived() {
+        return archived;
+    }
+
+    public OffsetDateTime registeredAt() {
+        return registeredAt;
+    }
+
+    public OffsetDateTime archivedAt() {
+        return archivedAt;
+    }
+
+    public Integer loyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    private void setId(UUID id) {
+        Objects.requireNonNull(id);
+        this.id = id;
+    }
+
+    private void setFullName(String fullName) {
+        Objects.requireNonNull(fullName, VALIDATION_ERROR_FULLNAME_IS_NULL);
+        if(fullName.isBlank()) {
+            throw new IllegalArgumentException(VALIDATION_ERROR_FULLNAME_IS_BLANK);
+        }
+        this.fullName = fullName;
+    }
+
+    private void setBirthDate(LocalDate birthDate) {
+        if (birthDate == null) {
+            this.birthDate = null;
+            return;
+        }
+        if (birthDate.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException(VALIDATION_ERROR_BIRTHDATE_MUST_IN_PAST);
+        }
+        this.birthDate = birthDate;
+    }
+
+    private void setEmail(String email) {
+        FieldValidations.requiresValidEmail(email, VALIDATION_ERROR_EMAIL_IS_INVALID);
+        this.email = email;
+    }
+
+    private void setPhone(String phone) {
+        Objects.requireNonNull(phone);
+        this.phone = phone;
+    }
+
+    private void setDocument(String document) {
+        Objects.requireNonNull(document);
+        this.document = document;
+    }
+
+    private void setPromotionNotificationsAllowed(Boolean promotionNotificationsAllowed) {
+        Objects.requireNonNull(promotionNotificationsAllowed);
+        this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+    }
+
+    private void setArchived(Boolean archived) {
+        Objects.requireNonNull(archived);
+        this.archived = archived;
+    }
+
+    private void setRegisteredAt(OffsetDateTime registeredAt) {
+        Objects.requireNonNull(registeredAt);
+        this.registeredAt = registeredAt;
+    }
+
+    private void setArchivedAt(OffsetDateTime archivedAt) {
+        this.archivedAt = archivedAt;
+    }
+
+    private void setLoyaltyPoints(Integer loyaltyPoints) {
+        Objects.requireNonNull(loyaltyPoints);
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/validator/FieldValidations.java
@@ -0,0 +1,25 @@
+package com.williamsilva.algashop.ordering.domain.validator;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.Objects;
+
+public class FieldValidations {
+
+    private FieldValidations() {
+    }
+
+    public static void requiresValidEmail(String email) {
+        requiresValidEmail(email, null);
+    }
+
+    public static void requiresValidEmail(String email, String errorMessage) {
+        Objects.requireNonNull(email, errorMessage);
+        if (email.isBlank()) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        if (!EmailValidator.getInstance().isValid(email)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+}
