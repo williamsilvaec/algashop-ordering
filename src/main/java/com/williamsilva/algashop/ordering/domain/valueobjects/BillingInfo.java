ddc96366ee56fa132f9e844b8aa8288478efbc96
William Silva <williamsilvaec@gmail.com>
2025-08-29 22:26:58 -0300

    feat: adicionar novos value objects

---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/valueobjects/BillingInfo.java
@@ -0,0 +1,21 @@
+package com.williamsilva.algashop.ordering.domain.valueobjects;
+
+import lombok.Builder;
+
+import java.util.Objects;
+
+@Builder
+public record BillingInfo(
+        FullName fullName,
+        Document document,
+        Phone phone,
+        Address address
+) {
+
+    public BillingInfo {
+        Objects.requireNonNull(fullName);
+        Objects.requireNonNull(document);
+        Objects.requireNonNull(phone);
+        Objects.requireNonNull(address);
+    }
+}
---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/valueobjects/ShippingInfo.java
@@ -0,0 +1,16 @@
+package com.williamsilva.algashop.ordering.domain.valueobjects;
+
+import lombok.Builder;
+
+import java.util.Objects;
+
+@Builder
+public record ShippingInfo(FullName fullName, Document document, Phone phone, Address address) {
+
+    public ShippingInfo {
+        Objects.requireNonNull(fullName);
+        Objects.requireNonNull(document);
+        Objects.requireNonNull(phone);
+        Objects.requireNonNull(address);
+    }
+}
