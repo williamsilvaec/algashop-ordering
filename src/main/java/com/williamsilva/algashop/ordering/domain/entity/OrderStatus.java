c030db4786515adff265807eaa964d59dc8c63c1
William Silva <williamsilvaec@gmail.com>
2025-09-02 20:01:28 -0300

    feat: Implementar Aggregate de Order

---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/entity/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.williamsilva.algashop.ordering.domain.entity;
+
+public enum OrderStatus {
+    DRAFT,
+    PLACED,
+    PAID,
+    READY,
+    CANCELED
+}
---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/entity/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.williamsilva.algashop.ordering.domain.entity;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    GATEWAY_BALANCE
+}
