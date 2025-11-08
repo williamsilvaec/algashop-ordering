931492addfd8c533b60a51a5b7b6b7b9d0403d5b
William Silva <williamsilvaec@gmail.com>
2025-11-08 10:56:56 -0300

    feat: Configurar auditoria na entidade Order

--- src/main/java/com/williamsilva/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntity.java
+++ src/main/java/com/williamsilva/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntity.java
@@ -1,6 +1,7 @@
 package com.williamsilva.algashop.ordering.infrastructure.persistence.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -24,6 +29,7 @@ import java.util.UUID;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString(of = "id")
 @Table(name = "\"order\"")
+@EntityListeners(AuditingEntityListener.class)
 public class OrderPersistenceEntity {
 
     @Id
@@ -39,4 +45,13 @@ public class OrderPersistenceEntity {
     private OffsetDateTime paidAt;
     private OffsetDateTime canceledAt;
     private OffsetDateTime readyAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+
+    @LastModifiedDate
+    private OffsetDateTime lastModifiedAt;
+
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
 }
--- src/test/java/com/williamsilva/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/williamsilva/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,5 +1,6 @@
 package com.williamsilva.algashop.ordering.infrastructure.persistence.repository;
 
+import com.williamsilva.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.williamsilva.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import com.williamsilva.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityTestDataBuilder;
 import org.assertj.core.api.Assertions;
@@ -7,9 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(SpringDataAuditingConfig.class)
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;
@@ -32,6 +35,16 @@ class OrderPersistenceEntityRepositoryIT {
     void shouldCount() {
         long ordersCount = orderPersistenceEntityRepository.count();
         Assertions.assertThat(ordersCount).isZero();
+    }
+
+    @Test
+    void shouldSetAuditingValues() {
+        OrderPersistenceEntity entity = OrderPersistenceEntityTestDataBuilder.existingOrder().build();
+        entity = orderPersistenceEntityRepository.saveAndFlush(entity);
+
+        Assertions.assertThat(entity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedByUserId()).isNotNull();
     }
 
 }
