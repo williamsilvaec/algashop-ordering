8d05c3c4f2220e3a1cba1a0bf79891819550cd0d
William Silva <williamsilvaec@gmail.com>
2025-11-22 23:12:47 -0300

    feat: criar Domain Service de Shopping

---
+++ src/main/java/com/williamsilva/algashop/ordering/domain/model/customer/CustomerAlreadyHaveShoppingCartException.java
@@ -0,0 +1,6 @@
+package com.williamsilva.algashop.ordering.domain.model.customer;
+
+import com.williamsilva.algashop.ordering.domain.model.DomainException;
+
+public class CustomerAlreadyHaveShoppingCartException extends DomainException {
+}
