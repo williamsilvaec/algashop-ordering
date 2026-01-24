4823742f45cca2b1e2f388b884de65fc0aa0e8b9
William Silva <williamsilvaec@gmail.com>
2026-01-24 11:45:34 -0300

    feat: limpar dados de teste com flyway

--- src/test/java/com/williamsilva/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
+++ src/test/java/com/williamsilva/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
@@ -16,8 +16,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+//@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Transactional
 class CustomerQueryServiceIT {
 
     @Autowired
---
+++ src/test/resources/db/clean/afterMigrate.sql
@@ -0,0 +1,5 @@
+truncate table customer cascade;
+truncate table "order" cascade;
+truncate table order_item cascade;
+truncate table shopping_cart cascade;
+truncate table shopping_cart_item cascade;
